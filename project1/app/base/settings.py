7961da22440aa91a561827602a2c2b4e13b69fef
HBourne <540355517@qq.com>
2020-02-09 01:14:44 -0600

    [UPDATE] database connection established

--- project1/app/base/settings.py
+++ project1/app/base/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/Users/hankren/RQH/git/CS411/project1/app/base/mysql.conf',
+        },
     }
 }
 
