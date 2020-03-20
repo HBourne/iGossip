c8347d011c98f023a4bad936268cd9478fd55f22
Gaurav Sharma <gauravsharma524@gmail.com>
2020-03-19 17:26:45 -0700

    Backend API for course model

--- project1/app/apiTest/urls.py
+++ project1/app/apiTest/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/test', views.CourseListCreate.as_view()),
+    path('api/search/', views.CourseListFilter.as_view()),
+    
 ]
--- project1/app/apiTest/views.py
+++ project1/app/apiTest/views.py
@@ -1,22 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
-# from apiTest.serializers import ApiTestSerializer
 from apiTest.serializers import CourseSerializer
-# from apiTest.models import ApiTest
 from apiTest.models import Course
 from rest_framework import filters
+from rest_framework import viewsets
 
-# Create your views here.
-# class ApiTestListCreate(generics.ListCreateAPIView):
-#     queryset = ApiTest.objects.all()
-#     serializer_class = ApiTestSerializer
-#     filter_backends = [filters.SearchFilter]
-#     search_fields = ['name','email','message','created_at']
 
-class CourseListCreate(generics.ListCreateAPIView):
-    queryset=Course.objects.all()
-    serializer_class = CourseSerializer
+class CourseListFilter(generics.ListCreateAPIView):
+      serializer_class = CourseSerializer
+      
+      def get_queryset(self):
+        if self.request.method == 'GET':
+            queryset = Course.objects.all()
+            curr_id = self.request.GET.get('id',None)
+            if curr_id is not None:
+                queryset = queryset.filter(id = curr_id)
+                return queryset
+            else:    
+                curr_name = self.request.GET.get('name', None)
+                curr_number = self.request.GET.get('number',None)
+                curr_instructor = self.request.GET.get('instructor',None)
+                if curr_name is not None:
+                    queryset = queryset.filter(name = curr_name)
+                if curr_number is not None:
+                    queryset = queryset.filter(number = curr_number )
+                if curr_instructor is not None:
+                    queryset = queryset.filter(instructor = curr_instructor)
+                return queryset
 
 def index(request):
     return HttpResponse("Hello, world!")
