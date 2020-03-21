da408ab86c015efd2b55316e6249ae70493e81d9
Gaurav Sharma <gauravsharma524@gmail.com>
2020-03-20 19:07:36 -0700

    Changed url name

--- project1/app/apiTest/urls.py
+++ project1/app/apiTest/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/search/', views.CourseListGeneralSearch.as_view()),
+    path('courses/search/', views.CourseListGeneralSearch.as_view()),
     
 ]
