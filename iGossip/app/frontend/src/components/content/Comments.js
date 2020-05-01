588dd614922bde5abda1da56fd1929c637dc44cb
HBourne <540355517@qq.com>
2020-05-01 04:30:25 -0500

    [UPDATE] comments implemented

--- iGossip/app/frontend/src/components/content/Comments.js
+++ iGossip/app/frontend/src/components/content/Comments.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import './favorites.less'
-import { Table, Tag, Popconfirm, message } from 'antd';
+import './comments.less'
+import { List, message } from 'antd';
 import 'antd/dist/antd.css';
 import cookie from "react-cookies";
 import axios from 'axios';
@@ -19,6 +19,33 @@ export class Comments extends Component {
 
     componentDidMount() {
         // fetch comments to the course
+        this.fetchComment();
+    }
+
+    handleDelete = (hash_val) => {
+        axios.delete('http://127.0.0.1:3000/comment', {
+            data: {
+                user: cookie.load('username'),
+                hash_val: hash_val,
+            }
+        })
+            .then(res => {
+                if (res.status >= 400) {
+                    message.error(res.status)
+                    throw res
+                }
+
+                if (res.status == 200) {
+                    message.success("Successfully deleted comment")
+                    this.fetchComment();
+                }
+            })
+            .catch((err) => {
+                message.error("Error deleting comment. Please try again later...")
+            })
+    }
+
+    fetchComment = () => {
         axios.get('http://127.0.0.1:3000/comment?user=' + cookie.load('username'))
             .then((res) => {
                 if (res.status >= 400) {
@@ -28,49 +55,37 @@ export class Comments extends Component {
                 return res.data;
             })
             .then(data => {
-                let comments = [];
-                // for (let i = data.length - 1; i >= 0; i--) {
-                //     comments.push({
-                //         actions: data[i].user == cookie.load('username') ? [<Button onClick={this.handleDelete}>Delete</Button>] : null,
-                //         author: data[i].user,
-                //         content: (
-                //             <p>
-                //                 {data[i].content}
-                //             </p>
-                //         ),
-                //         datetime: (
-                //             <Tooltip
-                //                 title={moment(data[i].created_at).fromNow()}
-                //             >
-                //                 <span>
-                //                     {moment(data[i].created_at).fromNow()}
-                //                 </span>
-                //             </Tooltip>
-                //         ),
-                //     })
-                // }
                 this.setState({
-                    comments: comments
+                    comments: data
                 })
-                console.log(data);
             })
             .catch((err) => {
                 message.error("Error fetching comments");
             })
     }
 
-    handleClick = (key) => {
-
-    }
-
-    handleDelete = (key) => {
-
-    }
-
     render() {
         return (
             <div className='comments'>
-
+                <div className='title'>
+                    My Comments
+                </div>
+                <div className='comment_list'>
+                    <List
+                        itemLayout="horizontal"
+                        dataSource={this.state.comments}
+                        renderItem={item => (
+                            <List.Item
+                                actions={[<a onClick={() => this.handleDelete(item.hash_val)}>Delete</a>]}
+                            >
+                                <List.Item.Meta
+                                    title={item.course_subject + item.course_number + " " + item.course_name + " - " + item.instructor}
+                                />
+                                {item.content}
+                            </List.Item>
+                        )}
+                    />
+                </div>
             </div>
         )
     }
